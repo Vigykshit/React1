da2f5c41ae406f1fba3e2ad7674fe999c335fb0c
Vigykshit katoch <katoch.vigy96@gmail.com>
2023-10-24 18:20:09 +0530

    first commit

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -71,7 +71,7 @@ const Header = () => {
                             <ul className="navbar-nav me-auto">
                                 <li className="nav-item">
                                     <Link to="#" className="nav-link"/>
-                                        <button onChange={geolocation()} className="a2"><i className="bi bi-geo-alt-fill"></i><p id="weather"></p></button>
+                                        <button onClick={geolocation()} className="a2"><i className="bi bi-geo-alt-fill"></i><p id="weather"></p></button>
                                     
                                 </li>
                             </ul>
@@ -79,7 +79,7 @@ const Header = () => {
                                 <input className="form-control me-2 box " type="text" placeholder="search for products"></input>
                                 <button className="btn btn-primary " type="button"><i className="bi bi-search"></i></button>
                             </form>
-                            <button className="btn btn-primary f2"><i className="bi bi-sun"></i></button>
+                            {/* <button onClick={changeMode()} className="btn btn-primary f2 mydark"><i className="bi bi-sun"></i></button> */}
                             {ConditionalHeader()}
                            
                         </div>
@@ -89,18 +89,18 @@ const Header = () => {
                 <nav className="navbar navbar-expand-sm box1 ">
                     <ul className="nav">
                         <li className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">All</a>
+                        <Link to={`/listing/1`} className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">All</Link>
                             <ul className="dropdown-menu ">
-                                <li><a href="#" className="dropdown-item">Watch</a></li>
+                                <li><Link to={`/listing/1`}className="dropdown-item">Watch</Link></li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle box10" href="#" role="button" data-bs-toggle="dropdown">Fashion</a>
+                                <Link to={`/listing/1`} className="nav-link dropdown-toggle box10" href="#" role="button" data-bs-toggle="dropdown">Fashion</Link>
                                     <ul className="dropdown-menu ">
-                                        <li><a className="dropdown-item" href="#">Men's Cloths</a></li>
-                                        <li><a className="dropdown-item" href="#">Women's Cloths</a></li>
-                                        <li><a className="dropdown-item" href="#">Kid's Cloths</a></li>
+                                        <li><Link to={`/listing/1`}className="dropdown-item" >Men's Cloths</Link></li>
+                                        <li><Link to={`/listing/1`}className="dropdown-item">Women's Cloths</Link></li>
+                                        <li><Link to={`/listing/1`} className="dropdown-item" >Kid's Cloths</Link></li>
                                     </ul>
                                 </li>
-                                <li><a className="dropdown-item" href="#">Automotive essentials</a></li>
+                                <li><Link to={`/listing/3`}className="dropdown-item" href="#">Automotive essentials</Link></li>
                             </ul>
                         </li>
                         <li className="nav-item">
@@ -159,6 +159,12 @@ function showPosition(data){
       
     })
 }
+
+
+//   function changeMode(){
+//       let mybody = document.body;
+//       mybody.classList.toggle('mydark')
+// }
 
 
 
